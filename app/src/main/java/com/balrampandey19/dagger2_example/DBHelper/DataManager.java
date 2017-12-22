e514808ebcf991d2e1fe968fa0ebca2676cda036
Balram Pandey <balrampandey19@gmail.com>
2017-12-22 13:37:01 +0530

    DataManager

---
+++ app/src/main/java/com/balrampandey19/dagger2_example/DBHelper/DataManager.java
@@ -0,0 +1,43 @@
+package com.balrampandey19.dagger2_example.DBHelper;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.balrampandey19.dagger2_example.Annotations.ApplicationContext;
+import com.balrampandey19.dagger2_example.User;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class DataManager {
+
+    private Context mContext;
+    private DbHelper mDbHelper;
+    private SharedPrefsHelper mSharedPrefsHelper;
+
+    @Inject
+    public DataManager(@ApplicationContext Context context,
+                       DbHelper dbHelper,
+                       SharedPrefsHelper sharedPrefsHelper) {
+        mContext = context;
+        mDbHelper = dbHelper;
+        mSharedPrefsHelper = sharedPrefsHelper;
+    }
+
+    public void saveAccessToken(String accessToken) {
+        mSharedPrefsHelper.put(SharedPrefsHelper.PREF_KEY_ACCESS_TOKEN, accessToken);
+    }
+
+    public String getAccessToken(){
+        return mSharedPrefsHelper.get(SharedPrefsHelper.PREF_KEY_ACCESS_TOKEN, null);
+    }
+
+    public Long createUser(User user) throws Exception {
+        return mDbHelper.insertUser(user);
+    }
+
+    public User getUser(Long userId) throws Resources.NotFoundException, NullPointerException {
+        return mDbHelper.getUser(userId);
+    }
+}
